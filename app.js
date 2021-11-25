a1c489173732de1454df31d3fa34bbc4a187301c
Nishit Pandit <nishitpandit@ymail.com>
2021-11-25 19:11:12 +0530

    minor change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(session({
 }))
 
 const mongoose = require('mongoose')
-const mongoURL = 'mongodb+srv://nishit:iwp_project_db@cluster0.8ntrt.mongodb.net/IWP_Data?retryWrites=true&w=majority' 
+const mongoURL = //mongoDB Credentials
 mongoose
   .connect(mongoURL,{
     useNewUrlParser: true,
